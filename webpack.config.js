a109b12880ac904925a2827d7ded327082ce2be9
luchjr <rockwindsea@gmail.com>
2022-12-07 22:41:45 +0300

    update  assembly

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
 
 module.exports = {
     entry: path.join(__dirname, 'src', 'index.js'),
@@ -11,7 +13,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 use: 'babel-loader',
                 exclude: /node_modules/,
             },
@@ -24,11 +26,25 @@ module.exports = {
                     'sass-loader'
                 ],
             },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: path.join('icons', '[name].[contenthash][ext]'),
+                },
+            },
         ],
+    },
+    resolve: {
+        extensions: ["*", ".js", ".jsx"],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'template.html'),
+            template: path.join(__dirname, 'src', 'index.html'),
             filename: 'index.html',
         }),
         new MiniCssExtractPlugin({
@@ -38,5 +54,22 @@ module.exports = {
     devServer: {
         watchFiles: path.join(__dirname, 'src'),
         port: 9000,
+    },
+    optimization: {
+        minimizer: [
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.imageminMinify,
+                    options: {
+                        plugins: [
+                            ['gifsicle', { interlaced: true }],
+                            ['jpegtran', { progressive: true }],
+                            ['optipng', { optimizationLevel: 5 }],
+                            ['svgo', { name: 'preset-default' }],
+                        ],
+                    },
+                },
+            }),
+        ],
     },
 };
