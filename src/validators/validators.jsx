74738d1fe2bd0570fba57e7bb6b8a5dd99d273cf
Dmitry Dziraev <dziraevdmitry@gmail.com>
2022-12-11 15:25:50 +0300

    fix regular expression for validate email

--- src/validators/validators.jsx
+++ src/validators/validators.jsx
@@ -1,18 +1,18 @@
 export const required = (value) => {
-    if (value) return undefined;
-    return 'Required field';
-}
+  if (value) return undefined;
+  return 'Required field';
+};
 
 export const emailValidator = (value) => {
-    let email = value.trim();
-    const EMAIL_REGEXP = /^[a-zA-Z0-9]+([!"#$%&'()*+,-./:;<=>?[\]\\^_{}][a-z0-9]+)*@([a-z0-9-]+)\.([a-z]{2,})(\.[a-z]{2,})?$/iu;
-    let domain = email.split("@")[1].split(".")[0];
-    if (!EMAIL_REGEXP.test(email) || domain[0] === "-" || domain[domain.length-1] === "-") return 'Invalid email';
-    return undefined;
-}
+  let email = value.trim();
+  const EMAIL_REGEXP =
+    /^[a-zA-Z0-9]+([!"#$%&'()*+,-./:;<=>?[\]\\^_{}][a-z0-9]+)*@([a-z0-9]+(-[a-z0-9]+)?)\.([a-z]{2,})(\.[a-z]{2,})?$/iu;
+  if (!EMAIL_REGEXP.test(email)) return 'Invalid email';
+  return undefined;
+};
 
 const maxLengthCreator = (maxLength, message) => (value) => {
-    if (value.length > maxLength) return message;
-    return undefined;
-}
+  if (value.length > maxLength) return message;
+  return undefined;
+};
 export const emailLength = maxLengthCreator(50, 'Invalid email');
