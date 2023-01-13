82cab58158dae5079ecd33fb2d5f6ace333c2bdc
Alina Malashko <alinaa.mal03@gmail.com>
2023-01-13 20:11:16 +0300

    Fix styles for nav/logout links, tooltip, header

--- src/pages/PersonalDetails/PersonalDetails.jsx
+++ src/pages/PersonalDetails/PersonalDetails.jsx
@@ -21,7 +21,6 @@ import { Notification } from '@components/tooltip/Notification';
 import { getChangedValues } from '../../utils/getChangedValues';
 import { usePersonalDetails } from '@hooks/usePersonalDetails';
 import $api from '../../http/api';
-import info from '../../static/images/info.png';
 import styles from './PersonalDetails.module.scss';
 
 const PersonalDetails = (props) => {
@@ -39,7 +38,28 @@ const PersonalDetails = (props) => {
       <div className={styles.blockTitle}>
         <h2 className={styles.title}>Personal details</h2>
         <Notification>
-          <img src={info} alt='notification'></img>
+          <svg 
+            width='22'
+            height='22' 
+            viewBox='0 0 22 22'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path 
+              d='M10.1666 7.51195C9.9456 7.29092 9.82143 6.99115 9.82143 6.67857C9.82143 6.36599 9.9456 6.06622 10.1666 5.8452C10.3876 5.62417 10.6874 5.5 11 5.5C11.3126 5.5 11.6124 5.62417 11.8334 5.8452C12.0544 6.06622 12.1786 6.36599 12.1786 6.67857C12.1786 6.99115 12.0544 7.29092 11.8334 7.51195C11.6124 7.73297 11.3126 7.85714 11 7.85714C10.6874 7.85714 10.3876 7.73297 10.1666 7.51195Z'
+              fill='#B0B0B0'
+            />
+            <path 
+              d='M10.2143 9.625C10.2143 9.51696 10.3027 9.42857 10.4107 9.42857H11.5893C11.6973 9.42857 11.7857 9.51696 11.7857 9.625V16.3036C11.7857 16.4116 11.6973 16.5 11.5893 16.5H10.4107C10.3027 16.5 10.2143 16.4116 10.2143 16.3036V9.625Z'
+              fill='#B0B0B0'
+            />
+            <path 
+              fillRule='evenodd'
+              clipRule='evenodd'
+              d='M0 11C0 4.92545 4.92545 0 11 0C17.0746 0 22 4.92545 22 11C22 17.0746 17.0746 22 11 22C4.92545 22 0 17.0746 0 11ZM1.86607 11C1.86607 16.0433 5.9567 20.1339 11 20.1339C16.0433 20.1339 20.1339 16.0433 20.1339 11C20.1339 5.9567 16.0433 1.86607 11 1.86607C5.9567 1.86607 1.86607 5.9567 1.86607 11Z'
+              fill='#B0B0B0'
+            />
+          </svg>
         </Notification>
       </div>
       <Formik
