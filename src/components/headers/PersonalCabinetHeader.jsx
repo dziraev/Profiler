f914a423862336811358133f1a4abbe19d612af7
Dmitry Dziraev <dziraevdmitry@gmail.com>
2023-01-13 18:02:01 +0300

    fix: user greeting if the name is in the database

--- src/components/headers/PersonalCabinetHeader.jsx
+++ src/components/headers/PersonalCabinetHeader.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { selectNamePersonalDetails } from '../../pages/PersonalDetails/selectors';
+import { selectPersonalDetails } from '../../pages/PersonalDetails/selectors';
 import styles from './PersonalCabinetHeader.module.scss';
 
 const PersonalCabinetHeader = (props) => {
-  const name = useSelector(selectNamePersonalDetails);
+  const { name, userInDB } = useSelector(selectPersonalDetails);
   return (
     <div className={styles.CabinetHeader}>
       <h1 className={styles.CabinetHeader__title}>
-        {!!name && <span className={styles.CabinetHeader__welcome}>Hello, {name}!</span>}
+        {!!name && userInDB && (
+          <span className={styles.CabinetHeader__welcome}>Hello, {name}!</span>
+        )}
         Welcome to{' '}
-        <span className={styles.CabinetHeader__title_highlighted}>
-          Profiler's personal cabinet
-        </span>{' '}
+        <span className={styles.CabinetHeader__title_highlighted}>Profiler's personal cabinet</span>{' '}
         — an employment program for our best students
       </h1>
     </div>
