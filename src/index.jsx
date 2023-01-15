5f72955ff88b43957ab9f3171067bdd96ca6ce5f
Dmitry Dziraev <dziraevdmitry@gmail.com>
2023-01-15 15:27:14 +0300

    create variables.scss
    All scss variables are stored in varaibles.scss

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
-import './static/scss/fonts.scss';
 import './static/scss/global.scss';
+import './static/scss/fonts.scss';
+import './static/scss/variables.scss';
 
 const root = createRoot(document.getElementById('root'));
 
