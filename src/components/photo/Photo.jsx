00fe21d4618ef95d67eaecf79b321cc6bff8c145
Alina Malashko <alinaa.mal03@gmail.com>
2023-01-19 23:22:13 +0300

    PROF2022-917, 923

--- src/components/photo/Photo.jsx
+++ src/components/photo/Photo.jsx
@@ -1,26 +1,40 @@
 import React, { useState } from 'react';
+import PopUpUploadPhotoCV from '../popup/uploadPhoto/uploadPhotoCV/PopUpUploadPhotoCV';
+import PopUpUploadPhotoCabinet from '../popup/uploadPhoto/uploadPhotoCabinet/PopUpUploadPhotoCabinet';
 import styles from './Photo.module.scss';
 
 const Photo = (props) => {
-  const [page, setPage] = useState(props.page);
+  const [openModal, setOpenModal] = useState(false);
+  const close = () => setOpenModal(false);
   return (
-    <div className={`${styles.photo} ${page === 'cabinet' ? styles.Cabinet : styles.CV}`}>
-      <div className={`${styles.photo__button} ${page === 'cabinet' ? styles.Cabinet : styles.CV}`}>
-        <svg
-          width='12'
-          height='12'
-          viewBox='0 0 12 12'
-          fill='none'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <path
-            d='M10.5 6.75H6.75V10.5C6.75 10.6989 6.67098 10.8897 6.53033 11.0303C6.38968 11.171 6.19891 11.25 6 11.25C5.80109 11.25 5.61032 11.171 5.46967 11.0303C5.32902 10.8897 5.25 10.6989 5.25 10.5V6.75H1.5C1.30109 6.75 1.11032 6.67098 0.96967 6.53033C0.829018 6.38968 0.75 6.19891 0.75 6C0.75 5.80109 0.829018 5.61032 0.96967 5.46967C1.11032 5.32902 1.30109 5.25 1.5 5.25H5.25V1.5C5.25 1.30109 5.32902 1.11032 5.46967 0.96967C5.61032 0.829017 5.80109 0.75 6 0.75C6.19891 0.75 6.38968 0.829017 6.53033 0.96967C6.67098 1.11032 6.75 1.30109 6.75 1.5V5.25H10.5C10.6989 5.25 10.8897 5.32902 11.0303 5.46967C11.171 5.61032 11.25 5.80109 11.25 6C11.25 6.19891 11.171 6.38968 11.0303 6.53033C10.8897 6.67098 10.6989 6.75 10.5 6.75Z'
-            fill='#407BFF'
-          />
-        </svg>
+    <>
+      <div 
+        className={`${styles.photo} ${props.page === 'cabinet' ? styles.Cabinet : styles.CV}`} 
+        onClick={() => setOpenModal(true)}
+      >
+        <div className={`${styles.photo__button} ${props.page === 'cabinet' ? styles.Cabinet : styles.CV}`}>
+          <svg
+            width='12'
+            height='12'
+            viewBox='0 0 12 12'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M10.5 6.75H6.75V10.5C6.75 10.6989 6.67098 10.8897 6.53033 11.0303C6.38968 11.171 6.19891 11.25 6 11.25C5.80109 11.25 5.61032 11.171 5.46967 11.0303C5.32902 10.8897 5.25 10.6989 5.25 10.5V6.75H1.5C1.30109 6.75 1.11032 6.67098 0.96967 6.53033C0.829018 6.38968 0.75 6.19891 0.75 6C0.75 5.80109 0.829018 5.61032 0.96967 5.46967C1.11032 5.32902 1.30109 5.25 1.5 5.25H5.25V1.5C5.25 1.30109 5.32902 1.11032 5.46967 0.96967C5.61032 0.829017 5.80109 0.75 6 0.75C6.19891 0.75 6.38968 0.829017 6.53033 0.96967C6.67098 1.11032 6.75 1.30109 6.75 1.5V5.25H10.5C10.6989 5.25 10.8897 5.32902 11.0303 5.46967C11.171 5.61032 11.25 5.80109 11.25 6C11.25 6.19891 11.171 6.38968 11.0303 6.53033C10.8897 6.67098 10.6989 6.75 10.5 6.75Z'
+              fill='#407BFF'
+            />
+          </svg>
+        </div>
+        <p className={`${styles.photo__title} ${props.page === 'cabinet' ? styles.Cabinet : styles.CV}`}>Add your photo</p>
       </div>
-      <p className={`${styles.photo__title} ${page === 'cabinet' ? styles.Cabinet : styles.CV}`}>Add your photo</p>
-    </div>
+      {openModal && props.page !== 'cabinet' &&
+        <PopUpUploadPhotoCV close={close} />
+      }
+      {openModal && props.page === 'cabinet' &&
+        <PopUpUploadPhotoCabinet close={close} />
+      }
+    </>
   );
 };
 
---
+++ src/components/popup/uploadPhoto/uploadPhotoCV/PopUpUploadPhotoCV.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import styles from './PopUpUploadPhotoCV.module.scss';
+import incorrect from '../../../../static/images/incorrect-photo.png';
+import correct from '../../../../static/images/correct-photo.png';
+
+export const PopUpUploadPhotoCV = (props) => {
+  return (
+    <div 
+      className={styles.overlay} 
+      onClick={props.close}>
+      <div className={styles.modal}>
+        <div
+          className={styles.modal__close}
+          onClick={props.close}
+        >
+          <svg 
+            width='30' 
+            height='30' 
+            viewBox='0 0 30 30' 
+            fill='none' 
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <g 
+              clipPath='url(#clip0_293_2738)'>
+              <path 
+                d='M23.5165 8.68655L17.2031 15L23.5165 21.3135C23.8514 21.6483 24.0395 22.1025 24.0395 22.5761C24.0395 23.0497 23.8514 23.5039 23.5165 23.8388C23.1816 24.1737 22.7274 24.3619 22.2538 24.3619C21.7802 24.3619 21.326 24.1737 20.9911 23.8388L14.6777 17.5254L8.36422 23.8388C8.02933 24.1737 7.57513 24.3619 7.10153 24.3619C6.62792 24.3619 6.17372 24.1737 5.83884 23.8388C5.50395 23.5039 5.31581 23.0497 5.31581 22.5761C5.31581 22.1025 5.50395 21.6483 5.83884 21.3135L12.1523 15L5.83883 8.68655C5.50395 8.35166 5.31581 7.89746 5.31581 7.42386C5.31581 6.95025 5.50395 6.49605 5.83883 6.16117C6.17372 5.82628 6.62792 5.63814 7.10152 5.63814C7.57513 5.63814 8.02933 5.82628 8.36422 6.16117L14.6777 12.4746L20.9911 6.16117C21.326 5.82628 21.7802 5.63814 22.2538 5.63814C22.7274 5.63814 23.1816 5.82628 23.5165 6.16117C23.8514 6.49605 24.0395 6.95025 24.0395 7.42386C24.0395 7.89746 23.8514 8.35166 23.5165 8.68655V8.68655Z' 
+                fill='#25225D'
+              />
+            </g>
+            <defs>
+              <clipPath 
+                id='clip0_293_2738'>
+                <rect 
+                  width='30' 
+                  height='30' 
+                  fill='white'
+                />
+              </clipPath>
+            </defs>
+          </svg>
+        </div>
+        <div className={styles.modal__content}>
+          <h2 className={styles.modal__content__title}>
+            Please choose a new photo from gallery.
+          </h2>
+          <p>Acceptable formats: <span>jpeg, jpg, png</span>. Allowed size up to <span>5 MB</span>.</p>
+          <p><span>Recommendation</span>: Avoid using selfies, photos with friends, or photos in poorly lit areas. Choose a photo in business style, where your face is clearly visible and there are no distractions in the background.</p>
+        </div>
+        <div className={styles.modal__images}>
+          <div className={styles.modal__images__image}>
+            <img src={incorrect} alt='example' />
+            <div className={styles.cross} />
+          </div>
+          <div className={styles.modal__images__image}>
+            <img src={correct} alt='example' />
+            <div className={styles.check} />
+          </div>
+        </div>
+        <div className={styles.modal__button}>
+          <label htmlFor='file' className={styles.modal__button__label}>Add photo</label>
+          <input type='file' name='photoUuid' id='file'/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopUpUploadPhotoCV;
