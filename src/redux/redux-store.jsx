da194d2faafa808e2ee57bc124fc5b744b109623
Dmitry Dziraev <dziraevdmitry@gmail.com>
2023-01-24 19:46:35 +0300

    remove overflow-x:hidden in body

--- src/redux/redux-store.jsx
+++ src/redux/redux-store.jsx
@@ -34,8 +34,8 @@ const rootReducer = (state, action) => {
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
